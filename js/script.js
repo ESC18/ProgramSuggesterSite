15c4b8a540c49707db149e8768f9abfeb98c4513
ESC18 <ponderosaprintingus@gmail.com>
2023-06-02 18:53:40 -0700

    Add fully operational arithmetic. Create new Responsive styles,style buttons.

--- js/script.js
+++ js/script.js
@@ -13,41 +13,48 @@ window.onload = function () {
         let Python = 0;
         let Javascript = 0;
         let Swift = 0;
+        let Proper = 0;
         if (Platform === "Desktop") {
             Python += 1;
             Javascript += 1;
-            Swift -= 1;
+            Swift -= 5;
+            Proper += 1;
             document.getElementById("Platform").style.color = "green";
         } else if (Platform === "Mobile") {
-            Python -= 1;
+            Python -= 5;
             Javascript += 0;
-            Swift += 4;
+            Swift += 5;
+            Proper += 1;
             document.getElementById("Platform").style.color = "green";
         } else {
             document.getElementById("Platform").style.color = "red";
         }
         if (Beginner === "Yes") {
             Python += 1;
-            Javascript += 0;
+            Javascript -= 2;
             Swift += 1;
+            Proper += 1;
             document.getElementById("Beginner").style.color = "green";
         } else if (Beginner === "No") {
             Python += 0;
             Javascript += 0;
             Swift += 0;
+            Proper += 1;
             document.getElementById("Beginner").style.color = "green";
         } else {
             document.getElementById("Beginner").style.color = "red";
         }
         if (FrontBack === "Front") {
             Python -= 1;
-            Javascript += 3;
+            Javascript += 5;
             Swift += 1;
+            Proper += 1;
             document.getElementById("FrontBack").style.color = "green";
         } else if (FrontBack === "Back") {
-            Python += 3;
-            Javascript += 0;
-            Swift += 2;
+            Python += 5;
+            Javascript -= 0;
+            Swift += 3;
+            Proper += 1;
             document.getElementById("FrontBack").style.color = "green";
         } else {
             document.getElementById("FrontBack").style.color = "red";
@@ -56,27 +63,48 @@ window.onload = function () {
             Python += 1;
             Javascript += 1;
             Swift -= 1;
+            Proper += 1;
             document.getElementById("BusFun").style.color = "green";
         } else if (BusFun === "Business") {
             Python += 0;
             Javascript += 0;
             Swift += 0;
+            Proper += 1;
             document.getElementById("BusFun").style.color = "green";
         } else {
             document.getElementById("BusFun").style.color = "red";
         }
         if (LogicUI === "Logic") {
-            Python += 2;
+            Python += 3;
             Javascript -= 1;
-            Swift += 2;
+            Swift += 3;
+            Proper += 1;
             document.getElementById("LogicUI").style.color = "green";
         } else if (LogicUI === "UI") {
-            Python -= 1;
-            Javascript += 3;
+            Python -= 2;
+            Javascript += 4;
             Swift += 2;
+            Proper += 1;
             document.getElementById("LogicUI").style.color = "green";
         } else {
             document.getElementById("LogicUI").style.color = "red";
+        }
+
+
+
+
+        if (Proper === 5) {
+            if (Python > Swift && Python > Javascript) {
+                console.log('Python is the language for you!');
+            } else if (Swift > Python && Swift > Javascript) {
+                console.log('Swift is the language for you!');
+            } else if (Javascript > Swift && Javascript > Python) {
+                console.log('Javascript is the language for you!');
+            } else {
+                console.log("Something ain't addin up - try a different selection");
+            }
+        } else {
+            console.log("Something ain't addin up - try a different selection");
         }
         console.log(Python,Swift,Javascript)
     } 
