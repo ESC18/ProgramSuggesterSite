0a44b13754e9b0020d15bf90b70fe11098f4447a
ESC18 <ponderosaprintingus@gmail.com>
2023-06-02 22:32:35 -0700

    Add additional styling, clean up code

--- js/script.js
+++ js/script.js
@@ -1,98 +1,98 @@
 window.onload = function () {
     let form = document.querySelector("form");
-    console.log('this works 0')
     form.reset();
     form.onsubmit = function (event) {
-        console.log('this works 1')
         event.preventDefault();
-        let Platform = document.getElementById("Platform").value;
-        let Beginner = document.getElementById("Beginner").value;
-        let FrontBack = document.getElementById("FrontBack").value;
-        let BusFun = document.getElementById("BusFun").value;
-        let LogicUI = document.getElementById("LogicUI").value;
+        //Local Variables for Elements.
+        let Platform = document.getElementById("platform").value;
+        let Beginner = document.getElementById("beginner").value;
+        let FrontBack = document.getElementById("frontBack").value;
+        let BusFun = document.getElementById("busFun").value;
+        let LogicUI = document.getElementById("logicUI").value;
         let Python = 0;
         let Javascript = 0;
         let Swift = 0;
         let Proper = 0;
+        //Dropdown branches (Business)
         if (Platform === "Desktop") {
             Python += 1;
             Javascript += 1;
             Swift -= 5;
             Proper += 1;
-            document.getElementById("Platform").style.color = "green";
+            document.getElementById("platform").style.color = "green";
         } else if (Platform === "Mobile") {
             Python -= 5;
             Javascript += 0;
-            Swift += 5;
+            Swift += 7;
             Proper += 1;
-            document.getElementById("Platform").style.color = "green";
+            document.getElementById("platform").style.color = "green";
         } else {
-            document.getElementById("Platform").style.color = "red";
+            document.getElementById("platform").style.color = "red";
         }
         if (Beginner === "Yes") {
             Python += 1;
             Javascript -= 2;
             Swift += 1;
             Proper += 1;
-            document.getElementById("Beginner").style.color = "green";
+            document.getElementById("beginner").style.color = "green";
         } else if (Beginner === "No") {
             Python += 0;
             Javascript += 0;
             Swift += 0;
             Proper += 1;
-            document.getElementById("Beginner").style.color = "green";
+            document.getElementById("beginner").style.color = "green";
         } else {
-            document.getElementById("Beginner").style.color = "red";
+            document.getElementById("beginner").style.color = "red";
         }
         if (FrontBack === "Front") {
             Python -= 1;
             Javascript += 5;
             Swift += 1;
             Proper += 1;
-            document.getElementById("FrontBack").style.color = "green";
+            document.getElementById("frontBack").style.color = "green";
         } else if (FrontBack === "Back") {
             Python += 5;
             Javascript -= 0;
             Swift += 3;
             Proper += 1;
-            document.getElementById("FrontBack").style.color = "green";
+            document.getElementById("frontBack").style.color = "green";
         } else {
-            document.getElementById("FrontBack").style.color = "red";
+            document.getElementById("frontBack").style.color = "red";
         }
         if (BusFun === "Fun") {
             Python += 1;
             Javascript += 1;
             Swift -= 1;
             Proper += 1;
-            document.getElementById("BusFun").style.color = "green";
+            document.getElementById("busFun").style.color = "green";
         } else if (BusFun === "Business") {
             Python += 0;
             Javascript += 0;
             Swift += 0;
             Proper += 1;
-            document.getElementById("BusFun").style.color = "green";
+            document.getElementById("busFun").style.color = "green";
         } else {
-            document.getElementById("BusFun").style.color = "red";
+            document.getElementById("busFun").style.color = "red";
         }
         if (LogicUI === "Logic") {
             Python += 3;
             Javascript -= 1;
             Swift += 3;
             Proper += 1;
-            document.getElementById("LogicUI").style.color = "green";
+            document.getElementById("logicUI").style.color = "green";
         } else if (LogicUI === "UI") {
-            Python -= 2;
-            Javascript += 4;
+            Python -= 3;
+            Javascript += 6;
             Swift += 2;
             Proper += 1;
-            document.getElementById("LogicUI").style.color = "green";
+            document.getElementById("logicUI").style.color = "green";
         } else {
-            document.getElementById("LogicUI").style.color = "red";
+            document.getElementById("logicUI").style.color = "red";
         }
 
 
 
-
+        //Program chooser & UI branch.
         if (Proper === 5) {
             if (Python > Swift && Python > Javascript) {
                 console.log('Python is the language for you!');
